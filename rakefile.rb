329e63c053ad2bfee9b16928b289785795289095
tallbryan <b_d_williams@yahoo.com>
2012-12-11 04:09:04 -0800

    fixed rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,34 @@
-task :default => [:test]
+# encoding: utf-8
 
-task :test do
-  rspec "lib/musicscrape_spec.rb"
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
 end
+require 'rake'
+
+require 'jeweler'
+Jeweler::Tasks.new do |gem|
+  # gem is a Gem::Specification... see http://docs.rubygems.org/read/chapter/20 for more options
+  gem.name = "musicscrape"
+  gem.homepage = "http://github.com/tallbryan/musicscrape"
+  gem.license = "BOB"
+  gem.summary = "scrape this weeks shows off the stranger"
+  gem.description = "scrape this weeks shows off the stranger"
+  gem.email = "fake@fake.com"
+  gem.authors = ["tallbryan"]
+  # dependencies defined in Gemfile
+end
+Jeweler::RubygemsDotOrgTasks.new
+
+require 'rspec/core'
+require 'rspec/core/rake_task'
+RSpec::Core::RakeTask.new(:spec) do |spec|
+  spec.pattern = FileList['spec/**/*_spec.rb']
+end
+
+task :default => :spec
