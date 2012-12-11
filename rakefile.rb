2dd9cecfe55f421fa466ff6459ea616fd95a09ee
tallbryan <b_d_williams@yahoo.com>
2012-12-11 04:13:44 -0800

    changed rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -14,7 +14,7 @@ require 'rake'
 require 'jeweler'
 Jeweler::Tasks.new do |gem|
   # gem is a Gem::Specification... see http://docs.rubygems.org/read/chapter/20 for more options
-  gem.name = "musicscrape"
+  gem.name = "musicscrape-0.0.1"
   gem.homepage = "http://github.com/tallbryan/musicscrape"
   gem.license = "BOB"
   gem.summary = "scrape this weeks shows off the stranger"
